3476c6f3c902491dd9ca6b35f1ff0d666b8635c1
sashcallaysilva <sashcallaysilva@gmail.com>
2025-12-01 12:03:47 -0500

    main: Updated homepage config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PersonalWebsite/', // <--- this must match your repo name
 })
+
